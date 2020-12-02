27e793b027aede60160badf180a954d44af25ebc
nickupx <nick.upsky@gmail.com>
2020-12-02 10:41:21 -0800

    clean

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import * as $ from 'jquery'
-import Post from '@models/Post'
-// import json from './assets/json.json'
-// import xml from './assets/data.xml'
-// import csv from './assets/data.csv'
+import Post from './models/Post'
 import WebpackLogo from '@/assets/webpack-logo.png'
-import React from 'react'
-import {render} from 'react-dom'
-import './babel'
+
 import './styles/styles.css'
-import './styles/less.less'
-import './styles/scss.scss'
+
 
 const post = new Post('Webpack Post Title', WebpackLogo)
 $('pre').addClass('code').html(post.toString())
