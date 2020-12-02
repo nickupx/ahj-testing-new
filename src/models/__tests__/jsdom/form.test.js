69d73fe7b3428ec95bbca1e8f59fa37fca41142c
nickupx <nick.upsky@gmail.com>
2020-12-02 15:26:48 -0800

    commit

---
+++ src/models/__tests__/jsdom/form.test.js
@@ -0,0 +1,14 @@
+import CardWidget from '../../widget'
+
+const widget = new CardWidget()
+const input = widget.input
+const submit = widget.submit
+
+describe('Card validation form', () => {  
+
+test('Should display error', () => {
+  input.value = '4111111111111113'
+  submit.click()
+  expect(document.querySelector('#error-message').textContent).toBe('Your number is not a card number')
+  })
+})
